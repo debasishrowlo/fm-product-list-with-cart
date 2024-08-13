1a01c537bc47ca5c400857357efaa80d416786d4
debasishrowlo <debasishrowlo@gmail.com>
2024-08-14 04:55:46 +0530

    Add cart section styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,28 @@ import { useEffect } from "react"
 import data from "./data.json"
 
 function App() {
+  const cart = [
+    {
+      id: "abcd1",
+      name: "Classic Tiramisu",
+      price: 5.50,
+      quantity: 1,
+    },
+    {
+      id: "abcd2",
+      name: "Vanilla Bean Crème Brûlée",
+      price: 7.00,
+      quantity: 4,
+    },
+    {
+      id: "abcd3",
+      name: "Vanilla Panna Cotta",
+      price: 6.50,
+      quantity: 2,
+    },
+  ]
+  const isCartEmpty = cart.length === 0
+
   useEffect(() => {
     document.body.classList.add("bg-rose-50")
 
@@ -35,11 +57,52 @@ function App() {
       </div>
       <div className="mt-32 shrink-0 lg:w-384 lg:mt-0 lg:px-0">
         <div className="p-24 bg-white rounded-12">
-          <p className="text-24 font-bold text-red">Your Cart (0)</p>
-          <div className="mt-24 py-16">
-            <img src="/assets/images/illustration-empty-cart.svg" className="mx-auto" alt="" />
-            <p className="mt-16 text-center text-14 font-semibold text-rose-500">Your added items will appear here</p>
-          </div>
+          <p className="text-24 font-bold text-red">Your Cart ({cart.length})</p>
+          {isCartEmpty ? (
+            <div className="mt-24 py-16">
+              <img src="/assets/images/illustration-empty-cart.svg" className="mx-auto" alt="" />
+              <p className="mt-16 text-center text-14 font-semibold text-rose-500">Your added items will appear here</p>
+            </div>
+          ) : (
+            <>
+              <div className="mt-8">
+                {cart.map(product => {
+                  const individualProductPrice = "@" + product.price.toFixed(2)
+                  const totalProductPrice = "$" + (product.price * product.quantity).toFixed(2)
+
+                  return (
+                    <div
+                      className="py-16 flex justify-between items-center border-t first:border-t-0 border-rose-100"
+                      key={product.id}
+                    >
+                      <div>
+                        <p className="text-14 font-semibold text-rose-900">{product.name}</p>
+                        <div className="mt-8 flex">
+                          <p className="text-14 font-semibold text-red">{product.quantity}x</p>
+                          <p className="ml-16 text-14 text-rose-500">{individualProductPrice}</p>
+                          <p className="ml-8 text-14 font-semibold text-rose-500">{totalProductPrice}</p>
+                        </div>
+                      </div>
+                      <button type="button" className="w-20 h-20 flex items-center justify-center border border-rose-400 rounded-full">
+                        <img src="/assets/images/icon-remove-item.svg" alt="" />
+                      </button>
+                    </div>
+                  )
+                })}
+              </div>
+              <div className="mt-8 pt-24 flex justify-between items-center border-t border-rose-100">
+                <p className="text-14 text-rose-900">Order total</p>
+                <p className="text-24 font-bold text-rose-900">$46.50</p>
+              </div>
+              <div className="mt-24 py-16 flex justify-center items-center bg-rose-50 rounded-8">
+                <img src="/assets/images/icon-carbon-neutral.svg" alt="" />
+                <p className="ml-8 text-14 text-rose-900">
+                  This is a <span className="font-semibold">carbon-neutral</span> delivery
+                </p>
+              </div>
+              <button type="button" className="w-full mt-24 py-16 bg-red font-semibold text-white rounded-full">Confirm Order</button>
+            </>
+          )}
         </div>
       </div>
     </div>
