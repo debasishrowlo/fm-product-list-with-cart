2a2edd550775e9e75b6438db17bcbc83070bb40d
debasishrowlo <debasishrowlo@gmail.com>
2024-08-15 01:06:21 +0530

    Create UI for confirm order dialog

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import resolveConfig from "tailwindcss/resolveConfig"
+import { Dialog, DialogPanel } from "@headlessui/react"
 
 import tailwindConfig from "@/../tailwind.config"
 import data from "./data.json"
@@ -24,11 +25,37 @@ type CartProduct = {
   id: string,
   name: string,
   price: number,
+  thumbnail: string,
   quantity: number,
 }
 
 function App() {
-  const [cart, setCart] = useState<CartProduct[]>([])
+  // const [cart, setCart] = useState<CartProduct[]>([])
+  const [cart, setCart] = useState<CartProduct[]>([
+    {
+      "id": "dc8691d4-c77d-4842-b857-b30db929e349",
+      "name": "Classic Tiramisu",
+      "price": 5.5,
+      "thumbnail": "./assets/images/image-tiramisu-thumbnail.jpg",
+      "quantity": 1
+    },
+    {
+      "id": "5a2599c8-0d6d-44ae-b329-1d7bf31ca762",
+      "name": "Vanilla Bean Crème Brûlée",
+      "price": 7,
+      "thumbnail": "./assets/images/image-creme-brulee-thumbnail.jpg",
+      "quantity": 4
+    },
+    {
+      "id": "cd48ed21-1141-4925-857f-79e346a799df",
+      "name": "Vanilla Panna Cotta",
+      "price": 6.5,
+      "thumbnail": "./assets/images/image-panna-cotta-thumbnail.jpg",
+      "quantity": 1
+    }
+  ])
+  const [confirmDialogOpen, setConfirmDialogOpen] = useState(true)
+
   const isCartEmpty = cart.length === 0
 
   const getTotalProductPrice = (product:CartProduct) => {
@@ -44,13 +71,65 @@ function App() {
         id: product.id,
         name: product.name,
         price: product.price,
+        thumbnail: product.image.thumbnail,
         quantity: 1,
       },
     ])
   }
 
-  const removeItem = (id:string) => {
-    setCart(cart.filter(product => product.id !== id))
+  const removeItem = (product:CartProduct) => {
+    setCart(cart.filter(cartProduct => cartProduct.id !== product.id))
+  }
+
+  const decrementQuantity = (product:Product) => {
+    const cartProductIndex = cart.findIndex(cartProduct => cartProduct.id === product.id)
+
+    if (cartProductIndex === -1) {
+      throw new Error("cart product not found")
+    }
+
+    const cartProduct = cart[cartProductIndex]
+
+    const newQuantity = cartProduct.quantity - 1
+
+    if (newQuantity === 0) {
+      removeItem(cartProduct)
+    } else {
+      setCart([
+        ...cart.slice(0, cartProductIndex),
+        {
+          ...cart[cartProductIndex],
+          quantity: newQuantity,
+        },
+        ...cart.slice(cartProductIndex + 1),
+      ])
+    }
+  }
+
+  const incrementQuantity = (product:Product) => {
+    const cartProductIndex = cart.findIndex(cartProduct => cartProduct.id === product.id)
+
+    if (cartProductIndex === -1) {
+      throw new Error("cart product not found")
+    }
+
+    const cartProduct = cart[cartProductIndex]
+
+    const newQuantity = cartProduct.quantity + 1
+
+    setCart([
+      ...cart.slice(0, cartProductIndex),
+      {
+        ...cart[cartProductIndex],
+        quantity: newQuantity,
+      },
+      ...cart.slice(cartProductIndex + 1),
+    ])
+  }
+
+  const startNewOrder = () => {
+    setCart([])
+    setConfirmDialogOpen(false)
   }
 
   useEffect(() => {
@@ -67,13 +146,14 @@ function App() {
         <h1 className="text-40 font-bold text-rose-900 lg:mt-0 lg:px-0">{data.category}</h1>
         <div className="mt-32 grid gap-y-24 md:grid-cols-3 md:gap-x-24 md:gap-y-32 lg:px-0">
           {data.products.map(product => {
-            const inCart = cart.some(cartProduct => cartProduct.id === product.id)
+            const cartProduct = cart.find(cartProduct => cartProduct.id === product.id) || null
+            const productAddedToCart = cartProduct !== null
 
             return (
               <div key={product.id}>
                 <div className="relative">
                   <div className="relative overflow-hidden rounded-8">
-                    {inCart && (
+                    {productAddedToCart && (
                       <div className="absolute inset-0 border-2 border-red rounded-8"></div>
                     )}
                     <picture>
@@ -82,14 +162,31 @@ function App() {
                       <img src={product.image.mobile} alt="" />
                     </picture>
                   </div>
-                  <button
-                    type="button"
-                    className="absolute top-full left-1/2 -translate-x-1/2 -translate-y-1/2 w-160 p-12 flex justify-center border border-rose-400 bg-white rounded-full"
-                    onClick={() => addItem(product)}
-                  >
-                    <img src="/assets/images/icon-add-to-cart.svg" alt="" />
-                    <span className="ml-8 text-14 font-semibold text-rose-900">Add to Cart</span>
-                  </button>
+                  <div className="absolute top-full left-1/2 -translate-x-1/2 -translate-y-1/2 w-160">
+                    <button
+                      type="button"
+                      className="w-full p-12 flex justify-center border border-rose-400 bg-white rounded-full"
+                      onClick={() => addItem(product)}
+                    >
+                      <img src="/assets/images/icon-add-to-cart.svg" alt="" />
+                      <span className="ml-8 text-14 font-semibold text-rose-900">Add to Cart</span>
+                    </button>
+                    {productAddedToCart && (
+                      <div className="absolute inset-0 flex justify-between items-center bg-red rounded-full">
+                        <button type="button" className="h-full px-12" onClick={() => decrementQuantity(product)}>
+                          <div className="w-20 h-20 flex items-center justify-center border border-white rounded-full">
+                            <img src="/assets/images/icon-decrement-quantity.svg" alt="" />
+                          </div>
+                        </button>
+                        <p className="text-white">{cartProduct.quantity}</p>
+                        <button type="button" className="h-full px-12" onClick={() => incrementQuantity(product)}>
+                          <div className="w-20 h-20 flex items-center justify-center border border-white rounded-full">
+                            <img src="/assets/images/icon-increment-quantity.svg" alt="" />
+                          </div>
+                        </button>
+                      </div>
+                    )}
+                  </div>
                 </div>
                 <div className="mt-38">
                   <p className="text-14 text-rose-500">{product.category}</p>
@@ -131,7 +228,7 @@ function App() {
                       <button
                         type="button"
                         className="w-20 h-20 flex items-center justify-center border border-rose-400 rounded-full"
-                        onClick={() => removeItem(product.id)}
+                        onClick={() => removeItem(product)}
                       >
                         <img src="/assets/images/icon-remove-item.svg" alt="" />
                       </button>
@@ -149,11 +246,62 @@ function App() {
                   This is a <span className="font-semibold">carbon-neutral</span> delivery
                 </p>
               </div>
-              <button type="button" className="w-full mt-24 py-16 bg-red font-semibold text-white rounded-full">Confirm Order</button>
+              <button
+                type="button"
+                className="w-full mt-24 py-16 bg-red font-semibold text-white rounded-full"
+                onClick={() => setConfirmDialogOpen(true)}
+              >
+                Confirm Order
+              </button>
             </>
           )}
         </div>
       </div>
+      <Dialog open={confirmDialogOpen} onClose={() => setConfirmDialogOpen(false)}>
+        <div className="fixed inset-0 bg-black/50">
+          <DialogPanel>
+            <div className="absolute bottom-0 w-full md:top-1/2 md:bottom-auto md:-translate-y-1/2 md:px-40 lg:max-w-592 lg:left-1/2 lg:-translate-x-1/2 lg:px-0">
+              <div className="max-h-[95vh] p-24 pt-40 bg-white rounded-12 overflow-y-auto md:p-40">
+                <img src="/assets/images/icon-order-confirmed.svg" alt="" />
+                <p className="mt-24 text-40 leading-tight font-bold text-rose-900">Order Confirmed</p>
+                <p className="mt-8 text-rose-500">We hope you enjoy your food!</p>
+                <div className="mt-32 p-24 bg-rose-50 rounded-8">
+                  {cart.map(product => {
+                    const totalProductPrice = getTotalProductPrice(product)
+
+                    return (
+                      <div key={product.id} className="py-16 first:pt-0 last:pb-0 flex items-center border-t first:border-t-0 border-rose-100">
+                        <img src={product.thumbnail} className="w-48 rounded-4" alt="" />
+                        <div className="ml-16 grow overflow-hidden">
+                          <p className="whitespace-nowrap overflow-hidden text-ellipsis text-14 font-semibold text-rose-900">
+                            {product.name}
+                          </p>
+                          <div className="mt-8 flex items-center">
+                            <p className="text-14 font-semibold text-red">{product.quantity}x</p>
+                            <p className="ml-8 text-14 text-rose-500">@{product.price.toFixed(2)}</p>
+                          </div>
+                        </div>
+                        <div className="ml-8 shrink-0 font-semibold text-rose-900">${totalProductPrice.toFixed(2)}</div>
+                      </div>
+                    )
+                  })}
+                  <div className="mt-24 pt-24 flex justify-between items-center border-t border-rose-100">
+                    <p className="text-14 text-rose-900">Order Total</p>
+                    <p className="text-24 font-bold text-rose-900">${orderTotal.toFixed(2)}</p>
+                  </div>
+                </div>
+                <button
+                  type="button"
+                  className="w-full mt-32 py-16 bg-red font-semibold text-white rounded-full"
+                  onClick={() => startNewOrder()}
+                >
+                  Start New Order
+                </button>
+              </div>
+            </div>
+          </DialogPanel>
+        </div>
+      </Dialog>
     </div>
   )
 }
